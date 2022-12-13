e2d7b26b313d8c889b94ecd4f6b4b9eec43efdfc
victorgp01 <victorgonzalezpresinal@gmail.com>
2022-12-12 20:15:49 -0400

    second commit

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -6,21 +6,21 @@ const useCrud = () => {
   const [users, setUsers] = useState();
 
   const getAllUsers = () => {
-    const URL = "http://users-crud.academlo.tech/users/";
+    const URL = "https://users-crud.academlo.tech/users/";
     axios
       .get(URL)
       .then((res) => setUsers(res.data))
       .catch((err) => console.log(err));
   };
   const createNewUser = (data) => {
-    const URL = "http://users-crud.academlo.tech/users/";
+    const URL = "https://users-crud.academlo.tech/users/";
     axios
       .post(URL, data)
       .then(() => getAllUsers())
       .catch((err) => console.log(err));
   };
   const deleteUserById = (id) => {
-    const URL = `http://users-crud.academlo.tech/users/${id}/`;
+    const URL = `https://users-crud.academlo.tech/users/${id}/`;
     axios
       .delete(URL)
       .then(() => getAllUsers())
@@ -28,7 +28,7 @@ const useCrud = () => {
     swal("User deleted successfully");
   };
   const updateUserByid = (id, data) => {
-    const URL = `http://users-crud.academlo.tech/users/${id}/`;
+    const URL = `https://users-crud.academlo.tech/users/${id}/`;
     axios
       .put(URL, data)
       .then((res) => getAllUsers())
